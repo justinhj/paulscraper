6efdc40ac171ca19986215261511d8bff2ff2d50
Justin Heyes-Jones <justinhj@gmail.com>
2023-08-04 20:37:23 -0700

    it works

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from "axios";
 import { JSDOM } from 'jsdom';
+import { promises as fs } from 'fs';
 
 function fetchPage(url: string): Promise<string | undefined> {
   const HTMLData = axios
@@ -32,7 +33,7 @@ async function getArticles() {
         const href: string | null = anchor.getAttribute('href');
         if (href) {
           if (href.startsWith('https')) {
-            urls.push(href);
+            // urls.push(href);
           } else {
             urls.push(`${baseUrl}${href}`);
           }
@@ -43,7 +44,30 @@ async function getArticles() {
   return urls;
 }
 
+async function getArticleFromUrl(url: string) {
+  const page = await fetchPage(url);
+  const dom = new JSDOM(page);
+  const doc = dom.window.document;
+  const font = doc.getElementsByTagName('font');
+  const text = font[0].textContent;
+  return text;
+}
+
+async function collectEssaysToFile(urls: string[], fileHandle: fs.FileHandle) {
+  for(const url of urls) {
+    let article= await getArticleFromUrl(url);
+    if(article) {
+      await fileHandle.write(`Essay from ${url}\n`);
+      await fileHandle.write(article);
+      console.log(`wrote essay from ${url}`);
+    }
+  }
+}
+
 (async () => {
-  let page = await getArticles();
-  console.log(page);
+  const fileHandle = await fs.open('./pg-essays.txt', 'w');
+  let urls = await getArticles();
+  console.log(`Got ${urls.length} article urls`);
+  await collectEssaysToFile(urls, fileHandle);
+  await fileHandle.close();
 })();
